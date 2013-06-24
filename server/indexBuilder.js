3835a02c275bc12a96d4efada608b95c1a129cad
Sheefeni Hauwanga <shef.hauwanga@gmail.com>
2013-06-23 20:50:12 -0400

    Fixed behaviors of servers.

--- server/indexBuilder.js
+++ server/indexBuilder.js
@@ -1,40 +1,36 @@
 var restRequest = require('restler');
 var redis = require("redis");
-var client = redis.createClient();
 var country_data = require("./countries");
+var country_to_currency = require("./currency_list");
+var client = redis.createClient();
+var hour = 1000 * 60 * 60;
+var check_list = {};
 
-function parse_list(currency_list) {
+function parse_list(currency_list, cache_list) {
     for(var i = 0; i < currency_list.length; i += 1) {
         if (currency_list[i].currency !== '') {
             var first_abbrev = currency_list[i].currency.split(",")[0];
             for(var j = 0; j < currency_list.length; j += 1) {
                 if (currency_list[j].currency !== '') {
                     var second_abbrev = currency_list[j].currency.split(",")[0];
-                    var currency_URI = 'http://rate-exchange.appspot.com/currency?from=' + first_abbrev + '&to=' + second_abbrev;
-                    restRequest.get(currency_URI).on('complete', function (data) {
-                        if (data.err === undefined && data.to !== undefined && data.from !== undefined) {
-                            currency_object = {
-                                //"key": data.from + data.to,
-                                "rate": data.rate,
-                                "base": find_country(data.from, currency_list),
-                                "to": find_country(data.to, currency_list),
-                            };
-                            client.set(data.from + data.to, JSON.stringify(currency_object));
-                            //console.log(JSON.stringify(currency_object));
-                        }
-                    });
+                    if (!(first_abbrev + second_abbrev in cache_list) || (Date.now() - cache_list[first_abbrev + second_abbrev]) < hour) {
+                        var currency_URI = 'http://rate-exchange.appspot.com/currency?from=' + first_abbrev + '&to=' + second_abbrev;
+                        restRequest.get(currency_URI).on('complete', function (data) {
+                            if (data.err === undefined && data.to !== undefined && data.from !== undefined) {
+                                cache_list[first_abbrev + second_abbrev] = Date.now();
+                                currency_object = {
+                                    "rate": data.rate,
+                                    "from": data.from,
+                                    "to": data.to,
+                                };
+                                client.set(data.from + data.to, JSON.stringify(currency_object));
+                            }
+                        });
+                    }
                 }
             }
         }
     }
 }
 
-function find_country(currency_abrev, currency_array) {
-    for (var i = 0; i < currency_array.length; i += 1) {
-        if (currency_array[i].currency.split(",")[0] === currency_abrev) {
-            return currency_array[i].name;
-        }
-    }
-}
-
-parse_list(country_data.currency_list);
+setInterval(parse_list(country_data.currency_list, check_list), hour/2);
--- server/mapWebServer.js
+++ server/mapWebServer.js
@@ -5,35 +5,35 @@ var country_data = require("./countries");
 
 var client = redis.createClient();
 var listenPort = 8888;
-var countries = []
+var conversions = []
 
 webServer.createServer(function (request, response) {
-    var country_obj = []
+    var conversion_obj = []
     var request_data = url.parse(request.url, true);
     var query = request_data.query;
-    var country_abrev = country_to_abbrev(query.base);
-    client.keys(country_abrev + "*", function (error, replies) {
-        replies.foreach (function (keyVal) {
+    var country_abbrev = country_to_abbrev(query.base, country_data.currency_list);
+    client.keys(country_abbrev + "*", function (error, replies) {
+        replies.forEach (function (keyVal) {
             client.get(keyVal.toString(), function (err, reply) {
-                 coutries.push(JSON.parse(reply)); 
+                 conversions.push(JSON.parse(reply)); 
             });
         });
     });
-    country_obj = countries;
-    counties = [];
+    conversion_obj = conversions;
+    conversions = [];
     var headers = {
         'Content-Type': 'text/plain',
-        'Content-Length': JSON.stringify(country_obj).length
+        'Content-Length': JSON.stringify(conversion_obj).length
     };
     response.writeHead(200, headers);
-    response.write(JSON.stringify(country_obj));
+    response.write(JSON.stringify(conversion_obj));
     response.end();
 }).listen(listenPort);
 
-function country_to_abbrev (country_name, currency_list) {
-    for (var i = 0; i < currency_list.length; i++) {
-        if (currency_list[i].name === country_name) {
-            return currency_list[i].currency.split(",")[0];
+function country_to_abbrev (country, country_list) {
+    for (var i = 0; i < country_list.length; i++) {
+        if (country_list[i].name === country) {
+            return country_list[i].currency.split(",")[0];
         }
     }
 }
